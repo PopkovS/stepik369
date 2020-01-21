dea5f0c75f24fa0fe00e5773ebfbd924e0fa881a
Sergei <PopkovSergei0805@yandex.ru>
2020-01-21 18:34:11 +0300

    homework_complete_2.0

--- test_items.py
+++ test_items.py
@@ -1,8 +1,10 @@
-from time import sleep
-
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_basket(browser):
     browser.get(link)
-    browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
+    button = len(browser.find_elements_by_css_selector("#id_quantity+.btn.btn-lg.btn-primary.btn-add-to-basket"))
+    assert button > 0, "Button \"Add to basket\" not found"
+
+
+"""Подсмотрел именно такой вариан проверки ассёртом в комментариях, помоему не плохой"""
